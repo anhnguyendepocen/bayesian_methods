de17cafcb19b75ee015120658619e1a101904ede
bparment1 <benoit.parmentier@gmail.com>
2018-04-25 14:30:00 -0400

    glmer code documentation and clean up

--- glmer_exploration.R
+++ glmer_exploration.R
@@ -1,4 +1,119 @@
-#######
+############### SESYNC Research Support: bayesian_Drosophila_PERA_analysis ########## 
+## Exploration of solutions for glmer with bayesian approach.
+## 
+## DATE CREATED: 04/24/2018
+## DATE MODIFIED: 04/24/2018
+## AUTHORS: Benoit Parmentier 
+## PROJECT: bayesian_Drosophila_PERA_analysis/
+## ISSUE: 
+## TO DO:
+##
+## COMMIT: testing simple regression with bayesion framework
+##
+## Links to investigate:
+#http://julianfaraway.github.io/brinla/
+
+###################################################
+#
+
+###### Library used
+
+library(gtools)                              # loading some useful tools 
+library(sp)                                  # Spatial pacakge with class definition by Bivand et al.
+library(spdep)                               # Spatial pacakge with methods and spatial stat. by Bivand et al.
+library(rgdal)                               # GDAL wrapper for R, spatial utilities
+library(raster)
+library(gdata)                               # various tools with xls reading, cbindX
+library(rasterVis)                           # Raster plotting functions
+library(parallel)                            # Parallelization of processes with multiple cores
+library(maptools)                            # Tools and functions for sp and other spatial objects e.g. spCbind
+library(maps)                                # Tools and data for spatial/geographic objects
+library(plyr)                                # Various tools including rbind.fill
+library(spgwr)                               # GWR method
+library(rgeos)                               # Geometric, topologic library of functions
+library(gridExtra)                           # Combining lattice plots
+library(colorRamps)                          # Palette/color ramps for symbology
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+library(rowr)                                # Contains cbind.fill
+library(car)
+library(sf)
+library(brinla)
+library(INLA)
+
+
+###### Functions used in this script and sourced from other files
+
+create_dir_fun <- function(outDir,out_suffix=NULL){
+  #if out_suffix is not null then append out_suffix string
+  if(!is.null(out_suffix)){
+    out_name <- paste("output_",out_suffix,sep="")
+    outDir <- file.path(outDir,out_name)
+  }
+  #create if does not exists
+  if(!file.exists(outDir)){
+    dir.create(outDir)
+  }
+  return(outDir)
+}
+
+#Used to load RData object saved within the functions produced.
+load_obj <- function(f){
+  env <- new.env()
+  nm <- load(f, env)[1]
+  env[[nm]]
+}
+
+### Other functions ####
+
+#function_processing_data <- ".R" #PARAM 1
+#script_path <- "/nfs/bparmentier-data/Data/projects/bayesian_Drosophila_PERA_analysis/R_scripts" #path to script #PARAM 
+#source(file.path(script_path,function_processing_data)) #source all functions used in this script 1.
+
+############################################################################
+#####  Parameters and argument set up ###########
+
+out_suffix <- "connectivity_example_04242018" #output suffix for the files and ouptut folder #param 12
+
+in_dir <- "/nfs/bparmentier-data/Data/projects/urban_garden_pursuit/data_urban_garden"
+out_dir <- "/nfs/bparmentier-data/Data/projects/urban_garden_pursuit/outputs"
+in_dir_grass <- "/nfs/bparmentier-data/Data/projects/urban_garden_pursuit/data_urban_garden" 
+#in_dir_grass <- "/nfs/bparmentier-data/Data/projects/urban_garden_pursuit/grass_data_urban_garden"
+
+# background reading:
+# https://grass.osgeo.org/grass72/manuals/grass_database.html
+
+gisBase <- '/usr/lib/grass72'
+#gisDbase <- '/nfs/urbangi-data/grassdata'
+gisDbase <- in_dir_grass #should be the same as in_dir
+
+#location <- 'DEM_LiDAR_1ft_2010_Improved_NYC_int'
+location <- 'NYC_example'
+location <- 'connectivy_example'
+
+file_format <- ".tif" #PARAM5
+NA_flag_val <- -9999 #PARAM7
+out_suffix <-"ny_example_04232018" #output suffix for the files and ouptu folder #PARAM 8
+create_out_dir_param=TRUE #PARAM9
+
+############## START SCRIPT ############################
+
+######### PART 0: Set up the output dir ################
+
+if(is.null(out_dir)){
+  out_dir <- in_dir #output will be created in the input dir
+  
+}
+#out_dir <- in_dir #output will be created in the input dir
+
+out_suffix_s <- out_suffix #can modify name of output suffix
+if(create_out_dir_param==TRUE){
+  out_dir <- create_dir_fun(out_dir,out_suffix)
+  setwd(out_dir)
+}else{
+  setwd(out_dir) #use previoulsy defined directory
+}
 
 
 
