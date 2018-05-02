4a5cc92771d890f04dc73401f37ba98fe080f19e
bparment1 <benoit.parmentier@gmail.com>
2018-05-02 14:30:00 -0400

    running poisson glm

--- poissonregression_malariacases.R
+++ poissonregression_malariacases.R
@@ -4,7 +4,7 @@
 #
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 04/25/2018 
-#DATE MODIFIED: 05/01/2018
+#DATE MODIFIED: 05/02/2018
 #Version: 1
 #PROJECT: India research from Neeti            
 
@@ -122,9 +122,10 @@ dim(rain_fall)
 View(rain_fall)
 dim(mal_inc)
 
-
+###### PART I: Reformat data and link to shapefile #################
 ### Need to combine both together.
-names(mal_inc)
+
+names(mal_inc)[1] <- "year"
 View(mal_inc)
 names(mal_inc)[1:5]
 
@@ -132,12 +133,6 @@ n <- ncol(mal_inc)
 n
 
 test <- as.data.frame(t(mal_inc[6:n]))
-#test2 <- as.data.frame(t(mal_inc[1:5]))
-
-data_df <- as.data.frame(t(mal_inc[,-1]))
-data_df$var <- rownames(data)
-names(data_df) <- 1994:2017
-View(data)
 names(mal_inc)[2:5]
 
 names(test) <- 1994:2017
@@ -151,105 +146,31 @@ names(mdata)[2] <- "year"
 View(mdata)
 names(mdata)[3] <- "mal_inc" 
 View(mdata)
-test <- merge(mdata,mal_inc[,1:5],by="year")
-View(test)
-#test3 <- gather(mal_inc[c(6:n)],state,names_state)
-#variable.name =c("state"),
-#value.names = c("time"))
+#### join climate indices by dates
+data_df <- merge(mdata,mal_inc[,1:5],by="year")
 
-#View(test3)
-#names_state <- names(mal_inc)[6:n]
-#var_names <- names(mal_inc)[2:5]
-#names(mal_inc)[1] <- "year"
-#test3 <- melt(mal_inc[c(1,6:n)],
-#        measure=var_names, 
-#        id=c("year",names_state))
-
-data_df <- t(test3)        
 View(data_df)
-View(test3)
-mdata_all <- melt(data, 
-              variable.name =c("state"),
-              value.name = c("time",names(mal_inc)[2:5]))
-View(data)
-View(mal_inc)
-test3 <- gather(mal_inc,state,names_state)
-View(test3)
-mdata <- melt(test, 
 
-dim(test)
-View(test2)
-test$state <- rownames(test)
-View(test)
+#mod_glm_poisson <- glm(mal_inc ~ ONI_DJF + DMI_ASO + MJO_DJFM + MJO_JJAS + rain_fall[,j], 
+#                       data=data_df,
+#                       family=poisson(link=log))
 
-names(test2) <- 1994:2017
-test2 <- test2[-1,]
-names(test) <- 1994:2017
-
-mdata <- melt(test, value.name=c("state","time"))
-
-test2$var <- rownames(test2)
-m_var <- melt(test2, 
-              #variable.name =c("state"),
-              value.names = c("time"))
-
-View(m_var)
-dim(mdata)
-class(mdata)
-names(mdata) <- c("state","year","mal_inc")
-View(mdata)
-24*36
+data_df$year <- as.numeric(data_df$year)
+class(data_df$state)
+class(data_df$ONI_DJF)
 
 
-lmp_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 6))
+mod_glm_poisson <- glm(mal_inc ~ state + year + ONI_DJF + DMI_ASO + MJO_DJFM + MJO_JJAS, 
+                       data=data_df,
+                       family=poisson(link=log))
+mod_glm_poisson
 
-<- vector(test)
 
-###### PART I: Reformat data and link to shapefile #################
-
-lmp_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 6))
-lmcoef_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 6))
-lmstd_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 6))
-lmcilow_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 6))
-lmciabove_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 6))
-aic_total <- array(data=NA, dim=c((ncol(rain_index)-1), 1))
-#lmp_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 3))
-#lmcoef_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 3))
-#lmstd_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 3))
-#lmcilow_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 3))
-#lmciabove_arr <- array(data=NA, dim=c((ncol(rain_index)-1), 3))
-
-#colnames(lmp_arr) <- c('intercept', 'oni', 'iod', 'ri')
-#colnames(lmcoef_arr) <- c('intercept', 'oni', 'iod', 'ri')
-#colnames(lmstd_arr) <- c('intercept', 'oni', 'iod', 'ri')
-#colnames(lmcilow_arr) <- c('intercept', 'oni', 'iod', 'ri')
-#colnames(lmciabove_arr) <- c('intercept', 'oni', 'iod', 'ri')
-
-colnames(lmp_arr) <- c('intercept', 'oni', 'iod','mjo_djfm', 'mjo_jjas', 'ri')
-colnames(lmcoef_arr) <- c('intercept', 'oni', 'iod', 'mjo_djfm', 'mjo_jjas','ri')
-colnames(lmstd_arr) <- c('intercept', 'oni', 'iod','mjo_djfm', 'mjo_jjas', 'ri')
-colnames(lmcilow_arr) <- c('intercept', 'oni', 'iod','mjo_djfm', 'mjo_jjas', 'ri')
-colnames(lmciabove_arr) <- c('intercept', 'oni', 'iod','mjo_djfm', 'mjo_jjas', 'ri')
-
-#colnames(lmp_arr) <- c('intercept', 'oni', 'iod',)
-#colnames(lmcoef_arr) <- c('intercept', 'oni', 'iod')
-#colnames(lmstd_arr) <- c('intercept', 'oni', 'iod')
-#colnames(lmcilow_arr) <- c('intercept', 'oni', 'iod')
-#colnames(lmciabove_arr) <- c('intercept', 'oni', 'iod')
-state_names <- colnames(rain_index)
-state_names <- state_names[-1]
-row.names(lmp_arr) <-state_names
-row.names(lmcoef_arr) <-state_names
-row.names(lmstd_arr) <-state_names
-row.names(lmcilow_arr) <-state_names
-row.names(lmciabove_arr) <-state_names
-row.names(aic_total) <-state_names
-mal_inc1 <- mal_inc[-c(30, 35,36,37,38,40)]
-for (i in 6:35){
-  j <- i -4
+#for (i in 6:35){
+#  j <- i -4
 #res <- glm(mal_inc1[,i] ~mal_inc1$ONI+mal_inc1$DMI+rain_index[,j], family=poisson(link=log))
   #res <- glm(mal_inc1[,i] ~mal_inc1$ONI+mal_inc1$DMI, family=poisson(link=log))
-res <- glm(mal_inc1[,i] ~mal_inc1$ONI_DJF+mal_inc1$DMI_ASO+mal_inc1$MJO_DJFM+mal_inc1$MJO_JJAS + rain_fall[,j], family=poisson(link=log))
+#res <- glm(mal_inc1[,i] ~mal_inc1$ONI_DJF+mal_inc1$DMI_ASO+mal_inc1$MJO_DJFM+mal_inc1$MJO_JJAS + rain_fall[,j], family=poisson(link=log))
 #res <- glm(mal_inc1[,i] ~mal_inc1$ONI_DJF+mal_inc1$DMI_ASO+ rain_fall[,j], family=poisson(link=log))
 
   #chaatisg_inc <- mal_inc1[,9]
@@ -257,28 +178,28 @@ res <- glm(mal_inc1[,i] ~mal_inc1$ONI_DJF+mal_inc1$DMI_ASO+mal_inc1$MJO_DJFM+mal
   
 #res <- glm(chaatisg_inc ~mal_inc1[(7:24),2]+mal_inc1[(7:24),3]+rain_fall[(7:24),6], family=poisson(link=log))
 
-res_sum <- summary(res)
-coef_res <- res_sum$coefficients[,1]
-p_res <- res_sum$coefficients[,4]
-stderror_coef <- res_sum$coefficients[,2]
-aic_model <- res_sum$aic
-aic_total[(i-5),] <- aic_model
-lmp_arr[(i-5),] <- p_res
-lmcoef_arr[(i-5),] <- exp(coef_res)
-lmstd_arr[(i-5),] <- stderror_coef
-lmcilow_arr[(i-5),] <- exp(coef_res - 1.96*stderror_coef)
-lmciabove_arr[(i-5),] <- exp(coef_res + 1.96*stderror_coef)
-#out <- capture.output(summary(res))
+#res_sum <- summary(res)
+# coef_res <- res_sum$coefficients[,1]
+# p_res <- res_sum$coefficients[,4]
+# stderror_coef <- res_sum$coefficients[,2]
+# aic_model <- res_sum$aic
+# aic_total[(i-5),] <- aic_model
+# lmp_arr[(i-5),] <- p_res
+# lmcoef_arr[(i-5),] <- exp(coef_res)
+# lmstd_arr[(i-5),] <- stderror_coef
+# lmcilow_arr[(i-5),] <- exp(coef_res - 1.96*stderror_coef)
+# lmciabove_arr[(i-5),] <- exp(coef_res + 1.96*stderror_coef)
+# #out <- capture.output(summary(res))
+# 
+# #cat("poisson regression analysis", out, file="summary_poissonregression.txt", sep="n", append=TRUE)
+# }
+#write.table(lmp_arr, 'pvalue_malpoisson_oni_iod94_rf_mjo.txt')
+#write.table(lmcoef_arr, 'coefvalue_malpoisson_oni_iod94_rf_mjo.txt')
+#write.table(lmstd_arr, 'stderror_malpoisson_oni_iod94_rf_mjo.txt')
+#write.table(lmcilow_arr, 'CIlow_malpoisson_oni_iod94_rf_mjo.txt')
+#write.table(lmciabove_arr, 'CIabove_malpoisson_oni_iod94_rf_mjo.txt')
 
-#cat("poisson regression analysis", out, file="summary_poissonregression.txt", sep="n", append=TRUE)
-}
-write.table(lmp_arr, 'pvalue_malpoisson_oni_iod94_rf_mjo.txt')
-write.table(lmcoef_arr, 'coefvalue_malpoisson_oni_iod94_rf_mjo.txt')
-write.table(lmstd_arr, 'stderror_malpoisson_oni_iod94_rf_mjo.txt')
-write.table(lmcilow_arr, 'CIlow_malpoisson_oni_iod94_rf_mjo.txt')
-write.table(lmciabove_arr, 'CIabove_malpoisson_oni_iod94_rf_mjo.txt')
-
-write.table(aic_total, 'aic_malpoisson_oni_iod_rf_mjo.txt')
+#write.table(aic_total, 'aic_malpoisson_oni_iod_rf_mjo.txt')
 
 
 ##################################  END OF SCRIPT #####################################
